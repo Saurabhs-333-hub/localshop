17d67e0c8f726e6891b8855871787e41c6538f9d
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2023-11-28 12:04:15 +0530

    commit

--- src/app/api/userauth/logout/route.ts
+++ src/app/api/userauth/logout/route.ts
@@ -7,9 +7,17 @@ export async function GET() {
             message: "Logout SuccessFull",
             success: true
         })
-        // localStorage.setItem("token", "")
-        res.cookies.delete("token")
-        console.log(res)
+        // localStorage.setItem("token", "hhghhj")
+        if (res.cookies.get("token") == null) {
+            console.log("null")
+        }
+        else {
+            res.cookies.delete("token")
+            console.log(`res${res.cookies.getAll().forEach((cookie) => {
+                console.log(cookie)
+            })}`)
+        }
+
         return res;
     } catch (error) {
         return NextResponse.json({
